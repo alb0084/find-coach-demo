888d134d414c68d17e08a5e247206195ce26c4ce
AF88405 <acerullo.ext@covercare.it>
2024-08-26 12:10:50 +0200

    find coach first commit demo app

---
+++ src/pages/coaches/CoachesRegistration.vue
@@ -0,0 +1,29 @@
+<template>
+    <section>
+        <base-card>
+            <div>
+                <h2>Register as Coach now!</h2>
+                <coach-form @save-data="saveData"></coach-form>
+            </div>
+        </base-card>
+    </section>
+</template>
+
+
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+    components: { CoachForm },
+    methods: {
+        saveData(data) {
+            this.$store.dispatch('coaches/registerCoach', data)
+            this.$router.replace('/coaches');
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* Aggiungi stili specifici qui */
+</style>
